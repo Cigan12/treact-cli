1e78ac6c7c6c41956bbf5ed984d61213f28bfd67
Cigan <darknew1998@mail.ru>
2021-02-04 16:08:52 +0700

    removed dist from gitIgnore

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const showLogo_1 = require("./showLogo/showLogo");
+showLogo_1.showLogo();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+#!/usr/bin/env node
 import { showLogo } from './showLogo/showLogo';
+
 showLogo();
