dacc0d00522eadf7babd81fd5f8eb29993f328ae
Cigan <darknew1998@mail.ru>
2021-02-04 15:27:38 +0700

    show logo completed

--- src/showLogo/showLogo.ts
+++ src/showLogo/showLogo.ts
@@ -1,5 +1,15 @@
 import figlet = require('figlet');
+import chalk = require('chalk');
+import clear = require('clear');
 
-const showLogo = () => {};
+const showLogo = (): void => {
+    clear();
+
+    console.log(
+        chalk.cyanBright(
+            figlet.textSync('TS-REACT', { horizontalLayout: 'full' })
+        )
+    );
+};
 
 export = showLogo;
