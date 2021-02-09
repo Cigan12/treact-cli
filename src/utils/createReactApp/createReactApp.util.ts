f96ad52d477a6e7bdc4f9ca9cb740b2c95534f87
Cigan <darknew1998@mail.ru>
2021-02-09 14:15:57 +0700

    questions inited

--- src/utils/createReactApp/createReactApp.util.ts
+++ src/utils/createReactApp/createReactApp.util.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 import { spawn } from 'child_process';
 import { ICreateReactAppProps } from './createReactApp.types';
 
-export const createReactApp = ({ name, type }: ICreateReactAppProps) => {
+export const createReactApp = ({ name, type }: ICreateReactAppProps): void => {
     if (type === 'spa') {
         const createApp = spawn('npx', [
             'create-react-app',
--- src/utils/makeInitQuestions/makeInitQuestions.util.ts
+++ src/utils/makeInitQuestions/makeInitQuestions.util.ts
@@ -1,6 +1,9 @@
 import inquirer from 'inquirer';
 import { createReactApp } from '../createReactApp/createReactApp.util';
-import { IQuestion } from '../makeQuestions/makeQuestions.types';
+import {
+    EPreprocessors,
+    IQuestion,
+} from '../makeQuestions/makeQuestions.types';
 
 export const createInitQuestions = (): void => {
     const questionsListForComponent: Array<IQuestion> = [
@@ -10,12 +13,64 @@ export const createInitQuestions = (): void => {
             message: 'Write project name',
         },
         {
+            type: 'input',
+            name: 'repository',
+            message: 'Write repository name or leave it empty',
+        },
+        {
             type: 'list',
             name: 'typeOfProject',
             message: 'Type of project?',
             choices: [
                 { name: 'Spa', value: 'spa' },
                 { name: 'Ssr', value: 'ssr' },
+            ],
+        },
+        {
+            type: 'list',
+            name: 'eslint',
+            message: 'Will you use ESLint',
+            choices: [
+                { name: 'Yes', value: 'yes' },
+                { name: 'No', value: 'no' },
+            ],
+        },
+        {
+            type: 'list',
+            name: 'preprocessor',
+            message: 'Which preprocessor you will use?',
+            choices: [
+                { name: 'SASS', value: EPreprocessors.SCSS },
+                { name: 'LESS', value: EPreprocessors.LESS },
+                { name: 'Stylus', value: EPreprocessors.STYLUS },
+                { name: 'None', value: EPreprocessors.NONE },
+            ],
+        },
+        {
+            type: 'list',
+            name: 'docker',
+            message: 'Will you use docker',
+            choices: [
+                { name: 'Yes', value: 'yes' },
+                { name: 'No', value: 'no' },
+            ],
+        },
+        {
+            type: 'list',
+            name: 'redux',
+            message: 'Will you use redux',
+            choices: [
+                { name: 'Yes', value: 'yes' },
+                { name: 'No', value: 'no' },
+            ],
+        },
+        {
+            type: 'list',
+            name: 'reduxSaga',
+            message: 'Will you use redux-saga',
+            choices: [
+                { name: 'Yes', value: 'yes' },
+                { name: 'No', value: 'no' },
             ],
         },
     ];
